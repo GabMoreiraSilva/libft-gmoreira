fa7d24aa1ac08d2809a244cc71f0596c7b9bde6a
gmoreira <gmoreira@student.42sp.org.br>
2022-05-12 02:21:57 +0200

    revisão 1

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmoreira <gmoreira@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 00:50:13 by gmoreira          #+#    #+#             */
-/*   Updated: 2022/05/10 01:09:54 by gmoreira         ###   ########.fr       */
+/*   Updated: 2022/05/12 02:16:05 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	*ft_calloc(size_t nb, size_t size)
 	size_t	check;
 
 	check = nb * size;
-	if (check / size != nb)
-		return (NULL);
 	if (nb == 0 || size == 0)
+		return (NULL);
+	if (check / size != nb)
 		return (NULL);
 	else
 	{
